306627841b5e8f020698f2a82a8b77507db363c2
namalxhero <nipunanamal66@gmail.com>
2026-08-07 19:18:16 +0530

    Update MyAdminReceiver.kt

--- app/src/main/java/com/example/lockdownapp/MyAdminReceiver.kt
+++ app/src/main/java/com/example/lockdownapp/MyAdminReceiver.kt
@@ -3,13 +3,16 @@ package com.example.lockdownapp
 import android.app.admin.DeviceAdminReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class MyAdminReceiver : DeviceAdminReceiver() {
     override fun onEnabled(context: Context, intent: Intent) {
         super.onEnabled(context, intent)
+        Toast.makeText(context, "Lockdown Protection Enabled", Toast.LENGTH_SHORT).show()
     }
 
     override fun onDisabled(context: Context, intent: Intent) {
         super.onDisabled(context, intent)
+        Toast.makeText(context, "Lockdown Protection Disabled", Toast.LENGTH_SHORT).show()
     }
 }
